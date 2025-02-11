59b7244d2aca5995733c74e447976cdf00c421bb
agustinEspinozaTech <agustinespinozatech@gmail.com>
2025-02-10 22:51:21 -0300

    v0.1.6

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import carritoRoutes from "./routes/carrito.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 1988;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
